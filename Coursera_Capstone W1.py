fac63371cbfb0e969dfe8d406c00a70ef41c3f0e
jacky883 <70831938+jacky883@users.noreply.github.com>
2020-09-07 12:00:59 -0700

    Add files via upload

---
+++ Coursera_Capstone W1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for Capstone Project Week 1 
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[7]:
+
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
